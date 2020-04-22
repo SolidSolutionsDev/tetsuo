f14ba4bb26d2856f8ac91936ba7fbc9f454529ac
João Faria <slimedev@gmail.com>
2020-04-22 23:20:49 +0100

    revert changes to examples and dist publishing for github pages

--- dist/uniforms.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * List of default uniforms passed to PostShaders and ShadedObjects
- */
-export declare const defaultUniforms: {
-    [key: string]: {
-        value: any;
-    };
-};
-//# sourceMappingURL=uniforms.d.ts.map
