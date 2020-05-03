ca10d5b3307babc2f77084de0a47fc3c4438350f
João Faria <slimedev@gmail.com>
2020-05-04 00:44:07 +0100

    add geometry loader

--- src/Loader.ts
+++ src/Loader.ts
@@ -1,14 +1,45 @@
 import * as THREE from "three";
 
+/**
+ * Wrapper singleton for all the relevant resource loaders
+ */
 export class Loader {
-    textureLoader: THREE.TextureLoader;
+    /**
+     * three.js texture loader
+     */
+    protected _textureLoader: THREE.TextureLoader;
+
+    /**
+     * three.js geometry JSON loader
+     */
+    protected _geometryLoader: THREE.BufferGeometryLoader;
 
     constructor() {
-        this.textureLoader = new THREE.TextureLoader();
+        this._textureLoader = new THREE.TextureLoader();
+        this._geometryLoader = new THREE.BufferGeometryLoader();
     }
 
+    /**
+     * Load a three.js texture located at a given url
+     *
+     * @param url
+     * @param onLoad
+     */
     loadTexture(url: string, onLoad?: (texture: THREE.Texture) => void) {
-        return this.textureLoader.load(url, onLoad);
+        return this._textureLoader.load(url, onLoad);
+    }
+
+    /**
+     * Loads a geometry from a JSON located at a given url
+     *
+     * @param url
+     * @param onLoad
+     */
+    loadGeometry(
+        url: string,
+        onLoad: (geometry: THREE.BufferGeometry | THREE.InstancedBufferGeometry) => void
+    ) {
+        return this._geometryLoader.load(url, onLoad);
     }
 }
 
