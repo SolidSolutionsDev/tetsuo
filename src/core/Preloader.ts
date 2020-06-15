b214d10e5e93d6ba0764705a2486fc48fb06a532
anticore <slimedev@gmail.com>
2020-06-16 00:51:36 +0100

    replace promise with callback for compatibility reasons

--- src/core/Preloader.ts
+++ src/core/Preloader.ts
@@ -3,39 +3,40 @@ import Loader from "./Loader";
 let assets: { [key: string]: any } = {};
 
 const Preloader = {
-    loadManifest: (manifestURL: string): Promise<any> =>
-        new Promise((resolve, reject) => {
-            fetch(manifestURL)
-                .then((response) => response.json())
-                .then((data) => {
-                    let itemCount = Object.keys(data).length;
+    loadManifest: (
+        manifestURL: string,
+        callback: (assets: any) => any
+    ): Promise<any> =>
+        fetch(manifestURL)
+            .then((response) => response.json())
+            .then((data) => {
+                let itemCount = Object.keys(data).length;
 
-                    function itemDone() {
-                        itemCount--;
-                        if (itemCount <= 0) {
-                            resolve(assets);
-                        }
+                function itemDone() {
+                    itemCount--;
+                    if (itemCount <= 0) {
+                        callback(assets);
                     }
+                }
 
-                    Object.keys(data).forEach((key) => {
-                        if (data[key].url.endsWith("mp3")) {
-                            Loader.loadAudio(data[key].url).then((audio) => {
-                                assets[key] = audio;
-                                itemDone();
-                            });
-                        } else if (data[key].url.endsWith("png")) {
-                            assets[key] = Loader.loadTexture(data[key].url);
+                Object.keys(data).forEach((key) => {
+                    if (data[key].url.endsWith("mp3")) {
+                        Loader.loadAudio(data[key].url).then((audio) => {
+                            assets[key] = audio;
                             itemDone();
-                        } else if (data[key].url.endsWith(".geo.json")) {
-                            Loader.loadGeometry(data[key].url, (geo) => {
-                                assets[key] = geo;
-                                itemDone();
-                            });
-                        }
-                    });
-                })
-                .catch((error) => reject(error));
-        }),
+                        });
+                    } else if (data[key].url.endsWith("png")) {
+                        assets[key] = Loader.loadTexture(data[key].url);
+                        itemDone();
+                    } else if (data[key].url.endsWith(".geo.json")) {
+                        Loader.loadGeometry(data[key].url, (geo) => {
+                            assets[key] = geo;
+                            itemDone();
+                        });
+                    }
+                });
+            })
+            .catch((error) => callback(error)),
 
     getAssetById: (id: string) => assets[id],
 };
