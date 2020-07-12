68d7369bfcda9dedd2f06689e8859fc70cb6c7fa
João Faria <slimedev@gmail.com>
2020-07-12 23:10:36 +0100

    add plane sdf

--- src/shaders/lib/raymarch/sdf.glsl
+++ src/shaders/lib/raymarch/sdf.glsl
@@ -12,6 +12,10 @@ float sdCylinder (vec3 point, vec3 c) {
   return length(point.xz - c.xy) - c.z;
 }
 
+float sdPlane (vec3 point, vec4 normal) {
+  	return dot(point, normal.xyz) + normal.w;  
+}
+
 float opSmoothUnion (float distance1, float distance2, float amount) {
     float h = clamp(0.5 + 0.5 * (distance2 - distance1) / amount, 0., 1.);
     return mix(distance2, distance1, h) - amount * h * (1. - h); 
